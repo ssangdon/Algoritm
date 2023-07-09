1b38b0e1a8c5ac0d1253510beef1588821a5fd97
ssangdon <tngustkdehs@naver.com>
2023-07-09 22:09:52 +0900

    2023/07/09_Solved

--- Level 2/124 나라의 숫자.js
+++
@@ -1,9 +0,0 @@
-function solution(n) {
-  var answer = "";
-  var numArr = ["4", "1", "2"];
-  while (n > 0) {
-    answer = numArr[n % 3] + answer;
-    n = parseInt((n - 1) / 3);
-  }
-  return answer.toString();
-}
--- Level 2/멀쩡한 사각형.js
+++
@@ -1,9 +0,0 @@
-const getGDC = (num1, num2) => {
-  return num2 > 0 ? getGDC(num2, num1 % num2) : num1;
-};
-
-function solution(w, h) {
-  let wholeNum = w * h;
-  let gdc = getGDC(h, w);
-  return wholeNum - (w + h - gdc);
-}
