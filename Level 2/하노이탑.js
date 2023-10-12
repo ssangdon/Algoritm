509a80d62da43dbe9e0e129c72eba1889e60bb9f
ssangdon <tngustkdehs@naver.com>
2023-10-12 18:58:06 +0900

    2023/10/12_Solved

---
+++ Level 2/하노이탑.js
@@ -0,0 +1,15 @@
+function solution(n) {
+  var answer = [];
+  let len = n;
+  const hanoi = (n, start, mid, end) => {
+    if (n === 1) {
+      answer.push([start, end]);
+    } else {
+      hanoi(n - 1, start, end, mid);
+      answer.push([start, end]);
+      hanoi(n - 1, mid, start, end);
+    }
+  };
+  hanoi(n, 1, 2, 3);
+  return answer;
+}
